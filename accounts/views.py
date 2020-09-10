2ae258fffdcd48e4724db064f65c75cbc485645e
abhay <abhaytiwari713@gmail.com>
2020-09-10 07:29:23 +0530

    removed register functionality

--- accounts/views.py
+++ accounts/views.py
@@ -23,34 +23,6 @@ def login(request):
 def dashboard(request):
     return render(request, "accounts/dashboard.html")
 
-def register(request):
-    if request.method == "POST":
-        first_name = request.POST["first_name"]
-        last_name = request.POST["last_name"]
-        username = request.POST["username"]
-        email = request.POST["email"]
-        password = request.POST["password"]
-        password2 = request.POST["password2"]
-
-        if password != password2:
-            messages.error(request, "passwords does not match.")
-
-        if User.objects.filter(username=username).exists():
-            messages.error(request, "username already exists.")
-            return redirect("register")
-        else:
-            if User.objects.filter(email=email).exists():
-                messages.error(request, "email already exists.")
-                return redirect("register")
-            else:
-                user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
-                user.save()
-                messages.success(request, "you are now registered and can login")
-                return redirect("login")
-
-        return render(request, "accounts/register.html")
-    return render(request, "accounts/register.html")
-
 
 def logout(request):
     if request.method == "POST":
